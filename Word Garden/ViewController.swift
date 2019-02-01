5dcce332c7ac2f2ff8878879a36e548cd035bd20
Mark Lee <lee.mark0320@gmail.com>
2019-01-31 23:03:56 -0500

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Mark Lee on 1/31/19.
+//  Copyright © 2019 Mark Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
